1692bef263ee07ca695aaf5aba87438080f440bb
Waldener Monteiro <waldener.dev@gmail.com>
2021-09-16 21:41:45 -0300

    refactor: finalizando a primeira etapa do app

--- trilha-react-native/myskills/src/App.js
+++ trilha-react-native/myskills/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
 import {Home} from './pages/Home';
 
 const App = () => {
-  return <Home />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#121015" />
+      <Home />
+    </>
+  );
 };
 
 export default App;
