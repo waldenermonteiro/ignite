0d6a3bf5c2064f15ee78865d5cd3bc2a6bc45865
Waldener Monteiro <waldener.dev@gmail.com>
2021-12-29 12:05:31 -0300

    feat: reinstalling project and finishing header component

--- trilha-react-native/rentx/src/screens/Home/index.tsx
+++ trilha-react-native/rentx/src/screens/Home/index.tsx
@@ -1,10 +1,24 @@
 import React from "react";
-import { Container, Title } from "./styles";
+import { StatusBar } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+
+import Logo from "../../assets/logo.svg";
+
+import { Container, Header, HeaderContent, TotalCars } from "./styles";
 
 export default function Home() {
   return (
     <Container>
-      <Title>Home</Title>
+      <StatusBar barStyle={"light-content"} backgroundColor={"transparent"} translucent />
+      <Header>
+        <HeaderContent>
+        <Logo width={RFValue(108)} height={RFValue(12)} />
+        <TotalCars>
+          Total de 12 carros
+        </TotalCars>
+        </HeaderContent>
+  
+      </Header>
     </Container>
   );
 }
