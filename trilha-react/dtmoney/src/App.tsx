610fe1deec15207cd8ebd827e2b2e3c4103c5028
Waldener Monteiro <waldener.dev@gmail.com>
2022-03-03 17:37:21 -0300

    finishing axios configuration

--- trilha-react/dtmoney/src/App.tsx
+++ trilha-react/dtmoney/src/App.tsx
@@ -1,15 +1,14 @@
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
 
-import styled from "styled-components";
-
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e6;
-`
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <Title>Hello World</Title>
-    </div>
+    <>
+      <Header />
+      <Dashboard />
+      <GlobalStyle />
+    </>
   );
 }
