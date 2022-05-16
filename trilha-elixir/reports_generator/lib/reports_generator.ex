56ea2aa05bd56d8cfaa8694f045385882299861b
Waldener Monteiro <waldener.dev@gmail.com>
2022-05-16 01:10:16 -0300

    refactor: lesson 7

--- trilha-elixir/reports_generator/lib/reports_generator.ex
+++ trilha-elixir/reports_generator/lib/reports_generator.ex
@@ -1,18 +1,20 @@
 defmodule ReportsGenerator do
-  @moduledoc """
-  Documentation for `ReportsGenerator`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ReportsGenerator.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def buid(filename) do
+    "reports/#{filename}"
+    |> File.stream!()
+    |> Enum.reduce(report_acc(), fn line, report ->
+      [id, _food_name, price] = parse_line(line)
+      Map.put(report, id, report[id] + price)
+    end)
   end
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    # |> List.update_at(2, fn elem -> String.to_integer(elem) end) : É a mesma coisa
+    |> List.update_at(2, &String.to_integer/1)
+  end
+
+  defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 end
