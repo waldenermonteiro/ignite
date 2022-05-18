2d2ac6364af57efb5017f52ae7501bc89af770ce
Waldener Monteiro <waldener.dev@gmail.com>
2022-05-18 01:08:27 -0300

    refactor: lesson 8

---
+++ trilha-elixir/reports_generator/lib/parser.ex
@@ -0,0 +1,15 @@
+defmodule ReportsGenerator.Parser do
+  def parse_file(filename) do
+    "reports/#{filename}"
+    |> File.stream!()
+    |> Stream.map(fn line -> parse_line(line) end)
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    # |> List.update_at(2, fn elem -> String.to_integer(elem) end) : É a mesma coisa
+    |> List.update_at(2, &String.to_integer/1)
+  end
+end
