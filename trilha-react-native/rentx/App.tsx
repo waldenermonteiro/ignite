1d9ce509955223db595ecc2979037c1358348743
Waldener Monteiro <waldener.dev@gmail.com>
2022-09-05 22:37:06 -0300

    feat: finishing first server side render with stripe api

--- trilha-react-native/rentx/App.tsx
+++ trilha-react-native/rentx/App.tsx
@@ -15,7 +15,7 @@ import {
   Archivo_600SemiBold,
 } from "@expo-google-fonts/archivo";
 
-import Home from "./src/screens/Home";
+import CarDetails from "./src/screens/CarDetails";
 
 import theme from "./src/styles/theme";
 
@@ -34,7 +34,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      <CarDetails />
     </ThemeProvider>
   );
 }
---
+++ trilha-react-native/rentx/src/screens/CarDetails/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Container, Header } from "./styles";
+
+export default function CarDetails() {
+  return (
+    <Container>
+      <Header></Header>
+    </Container>
+  );
+}
