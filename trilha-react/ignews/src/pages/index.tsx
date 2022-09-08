fa5ea344d45b525a331aab0fbb9ece02845d6f7d
Waldener Monteiro <waldener.dev@gmail.com>
2022-09-07 21:49:03 -0300

    refactor: using static render and using next api

--- trilha-react/ignews/src/pages/index.tsx
+++ trilha-react/ignews/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 
 import Head from "next/head";
 
@@ -39,7 +39,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1Kf6mLL8Z5pBp40NHg85ubDA");
 
   const product = {
@@ -52,5 +52,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
 
   return {
     props: { product },
+    revalidate: 60 * 60 * 24 // 24 hours
   };
 };
