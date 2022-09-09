5754a2fc0b3d7ddf9c3d37bcc48281918e940dd3
Waldener Monteiro <waldener.dev@gmail.com>
2022-09-09 20:10:24 -0300

    feat: using next-auth and creating login with github

--- trilha-react/ignews/src/components/Header/SignInButton/index.tsx
+++ trilha-react/ignews/src/components/Header/SignInButton/index.tsx
@@ -1,19 +1,26 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 
+import { useSession, signIn, signOut } from "next-auth/react"
+
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+
+  const { data: session } = useSession()
+
+  console.log(session)
+
+  const isUserLoggedIn = session;
 
   return isUserLoggedIn ? (
     <button type="button" className={styles.signInButton}>
       <FaGithub color="#04d361" />
-      Waldener Monteiro
-      <FiX color="#737380" className={styles.closeIcon} />
+      {session.user.name}
+      <FiX color="#737380" className={styles.closeIcon}  onClick={() => signOut()} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button type="button" className={styles.signInButton} onClick={() => signIn('github')}>
       <FaGithub color="#eba417" />
       Sign in with GitHub
     </button>
