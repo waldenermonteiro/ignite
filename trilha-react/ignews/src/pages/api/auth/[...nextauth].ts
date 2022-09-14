605bb57d364cb63e8ea1b6844683a22c46c47586
Waldener Monteiro <waldener.dev@gmail.com>
2022-09-14 20:36:17 -0300

    feat: using FaunaDB and save user in login

--- trilha-react/ignews/src/pages/api/auth/[...nextauth].ts
+++ trilha-react/ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,38 @@
 import NextAuth from "next-auth";
+
 import GithubProvider from "next-auth/providers/github";
+
+import { query as q } from "faunadb";
+
+import { fauna } from "../../../services/fauna";
 
 export const authOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: "read:user",
+        },
+      },
     }),
   ],
+  jwt: {
+    signingKey: process.env.SIGNING_KEY,
+  },
+  callbacks: {
+    async signIn(data) {
+      const { email } = data.user;
+      
+      try {
+        await fauna.query(q.Create(q.Collection('users'), { data: { email } }));
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 };
 
 export default NextAuth(authOptions);
