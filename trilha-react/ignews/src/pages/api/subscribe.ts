8cc2559408a3c3e16030cb12605cf2c446003445
Waldener Monteiro <waldener.dev@gmail.com>
2022-09-27 20:55:56 -0300

    refactor: removing duplicates

--- trilha-react/ignews/src/pages/api/subscribe.ts
+++ trilha-react/ignews/src/pages/api/subscribe.ts
@@ -1,33 +1,69 @@
 /* eslint-disable import/no-anonymous-default-export */
+
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
+import { query as q } from "faunadb";
 
+import { fauna } from "../../services/fauna";
 import { stripe } from "../../services/stripe";
+
+type User = {
+  ref: {
+    id: string;
+  };
+  data: {
+    stripe_customer_id: string;
+  };
+};
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const session = await getSession({ req });
 
-    console.log(session)
+    if (session) {
+  
+      console.log("SESSION", session);
 
-    const stripeCustomer = await stripe.customers.create({
-      email: session?.user?.email,
-    });
+      const user = await fauna.query<User>(
+        q.Get(
+          q.Match(q.Index("user_by_email"), q.Casefold(session?.user?.email))
+        )
+      );
+      console.log("USER", user);
 
-    const checkoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCustomer.id,
-      payment_method_types: ["card"],
-      billing_address_collection: "required",
-      line_items: [{ price: "price_1Kf6mLL8Z5pBp40NHg85ubDA", quantity: 1 }],
-      mode: "subscription",
-      allow_promotion_codes: true,
-      cancel_url: process.env.STRIPE_SUCCESS_URL,
-      success_url: process.env.STRIPE_CANCEL_URL,
-    });
+      let customerId = user.data.stripe_customer_id;
 
-    return res.status(200).json({ sessionId: checkoutSession.id });
-  } else {
-    res.setHeader("Allow", "POST");
-    res.status(405).end("Method not allowed");
+      if (!customerId) {
+        const stripeCustomer = await stripe.customers.create({
+          email: session?.user?.email,
+        });
+
+        await fauna.query(
+          q.Update(q.Ref(q.Collection("users"), user.ref.id), {
+            data: {
+              stripe_customer_id: stripeCustomer.id,
+            },
+          })
+        );
+
+        customerId = stripeCustomer.id;
+      }
+
+      const checkoutSession = await stripe.checkout.sessions.create({
+        customer: customerId,
+        payment_method_types: ["card"],
+        billing_address_collection: "required",
+        line_items: [{ price: "price_1Kf6mLL8Z5pBp40NHg85ubDA", quantity: 1 }],
+        mode: "subscription",
+        allow_promotion_codes: true,
+        cancel_url: process.env.STRIPE_SUCCESS_URL,
+        success_url: process.env.STRIPE_CANCEL_URL,
+      });
+
+      return res.status(200).json({ sessionId: checkoutSession.id });
+    } else {
+      res.setHeader("Allow", "POST");
+      res.status(405).end("Method not allowed");
+    }
   }
 };
